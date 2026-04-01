681bca868a972d6bb1d369ac5dc3a6f330091e04
xelitel <marat140606@gmail.com>
2026-04-01 19:45:28 +0300

    Static main title and sort list

--- src/widgets/Main/Main.jsx
+++ src/widgets/Main/Main.jsx
@@ -1,10 +1,13 @@
 import styles from './Main.module.css'
+import SortCardsForm from "@/features/sortingCards/index.js";
 
 const Main = () => {
   return (
     <main
       className={styles.main}
     >
+      <h1 className={styles.title}>Вишлисты</h1>
+      <SortCardsForm />
     </main>
   )
 }
