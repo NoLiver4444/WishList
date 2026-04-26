b9ef0369f4030296dc59c99a17d1d9fd0edec60a
XeliteL <marat140606@gmail.com>
2026-04-26 17:51:23 +0300

    Update docker

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -82,7 +82,7 @@ func New(
 
 func applyCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
